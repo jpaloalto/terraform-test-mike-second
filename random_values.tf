bdaefbef5cf7b6322254350db02c783af05841cc
jpaloalto <164952964+jpaloalto@users.noreply.github.com>
2024-04-09 13:10:39 -0700

    Update random_values.tf

--- random_values.tf
+++ random_values.tf
@@ -1,25 +1,25 @@
-resource "random_integer" "priority6" {
+resource "random_integer" "priority11" {
   min = 1
   max = 50000
 }
 
-resource "random_integer" "priority7" {
+resource "random_integer" "priority12" {
   min = 1
   max = 50000
 }
 
-resource "random_integer" "priority8" {
+resource "random_integer" "priority13" {
   min = 1
   max = 50000
 }
 
 
-resource "random_integer" "priority9" {
+resource "random_integer" "priority14" {
   min = 1
   max = 50000
 }
 
-resource "random_integer" "priority10" {
+resource "random_integer" "priority15" {
   min = 1
   max = 50000
 }
